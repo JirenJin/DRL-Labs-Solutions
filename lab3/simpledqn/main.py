ce97284d6d19fec75846cbe6a7bda483e33259af
Jiren Jin <jinjiren@foxmail.com>
2017-08-27 12:37:01 -0700

    finish double dqn

--- lab3/simpledqn/main.py
+++ lab3/simpledqn/main.py
@@ -208,8 +208,12 @@ class DQN(object):
         # Hint: You may want to make use of the following fields: self._discount, self._q, self._qt
         # Hint2: Q-function can be called by self._q.forward(argument)
         # Hint3: You might also find https://docs.chainer.org/en/stable/reference/generated/chainer.functions.select_item.html useful
-        loss = C.Variable(np.array([0.]))  # TODO: replace this line
         "*** YOUR CODE HERE ***"
+        a_prime = F.argmax(self._q.forward(l_next_obs), axis=1)
+        t = F.select_item(self._qt.forward(l_next_obs), a_prime)
+        target = l_rew + self._discount * t * (l_done == False)
+        current = F.select_item(self._q.forward(l_obs), l_act)
+        loss = F.mean_squared_error(current, target)
         return loss
 
     def train_q(self, l_obs, l_act, l_rew, l_next_obs, l_done):
